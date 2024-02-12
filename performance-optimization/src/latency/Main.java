9c6f9bdb187eb679d17276f513a4c6a2fb311b70
nasrmohammad4804 <nasrmohammad4804@gmail.com>
2024-02-12 06:39:16 -0800

    add generated flower picture after image processing

--- performance-optimization/src/latency/Main.java
+++ performance-optimization/src/latency/Main.java
@@ -3,43 +3,43 @@ package latency;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static latency.util.RGBUtility.*;
+import static latency.util.RGBUtility.changeRgbImage;
 
 public class Main {
-    private static String SOURCE_FILE="./resources/many-flowers.jpg";
-    private static String DESTINATION_FILE= "./generate/recolor.jpg";
+    private static String SOURCE_FILE = "./resources/many-flowers.jpg";
+    private static String DESTINATION_FILE = "./generate/recolor.jpg";
+
     public static void main(String[] args) throws IOException, InterruptedException {
 
         BufferedImage bufferedImage = ImageIO.read(new FileInputStream(SOURCE_FILE));
-        BufferedImage resultImage = new BufferedImage(bufferedImage.getWidth(),bufferedImage.getHeight(),BufferedImage.TYPE_INT_RGB);
+        BufferedImage resultImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-        long start  = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
 //        calculateWithSingleThread(bufferedImage, resultImage);
-        calculateWithMultiThread(13,bufferedImage,resultImage);
+        calculateWithMultiThread(13, bufferedImage, resultImage);
 
         long end = System.currentTimeMillis();
-        System.out.println("calculation takes : "+ (end - start));
+        System.out.println("calculation takes : " + (end - start));
     }
 
-    private static void calculateWithMultiThread(int numberOfThreads,BufferedImage bufferedImage, BufferedImage resultImage) throws InterruptedException, IOException {
+    private static void calculateWithMultiThread(int numberOfThreads, BufferedImage bufferedImage, BufferedImage resultImage) throws InterruptedException, IOException {
         List<Thread> threads = new ArrayList<>();
 
 
         final int EACH_LENGTH = bufferedImage.getHeight() / numberOfThreads;
 
-        for (int index = 0; index<numberOfThreads; index++){
+        for (int index = 0; index < numberOfThreads; index++) {
             threads.add(
                     new Thread(
                             new ImageWriter(
                                     (index * EACH_LENGTH),
-                                    (index+1) * EACH_LENGTH,
-                                    bufferedImage,resultImage
+                                    (index + 1) * EACH_LENGTH,
+                                    bufferedImage, resultImage
                             )
                     )
             );
@@ -51,17 +51,17 @@ public class Main {
         for (Thread thread : threads)
             thread.join();
 
-        ImageIO.write(resultImage,"jpg",new FileOutputStream(DESTINATION_FILE));
+        ImageIO.write(resultImage, "jpg", new FileOutputStream(DESTINATION_FILE));
 
     }
 
 
     private static void calculateWithSingleThread(BufferedImage bufferedImage, BufferedImage resultImage) throws IOException {
-            changeRgbImage(bufferedImage,0,bufferedImage.getHeight(),resultImage);
-            ImageIO.write(resultImage,"jpg",new FileOutputStream(DESTINATION_FILE));
+        changeRgbImage(bufferedImage, 0, bufferedImage.getHeight(), resultImage);
+        ImageIO.write(resultImage, "jpg", new FileOutputStream(DESTINATION_FILE));
     }
 
-    private static class ImageWriter implements Runnable{
+    private static class ImageWriter implements Runnable {
 
         private final int heightFrom;
         private final int heightTo;
@@ -70,7 +70,7 @@ public class Main {
         private final BufferedImage resultImage;
 
 
-        public ImageWriter( int heightFrom, int heightTo, BufferedImage originalImage, BufferedImage resultImage) {
+        public ImageWriter(int heightFrom, int heightTo, BufferedImage originalImage, BufferedImage resultImage) {
 
             this.heightFrom = heightFrom;
             this.heightTo = heightTo;
@@ -82,7 +82,7 @@ public class Main {
         @Override
         public void run() {
             try {
-                changeRgbImage(originalImage, heightFrom,heightTo,resultImage);
+                changeRgbImage(originalImage, heightFrom, heightTo, resultImage);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
